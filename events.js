df818dd160b2d049a4850de1453575b5b74b49ea
Majid Mohammed <majidmohammed044@gmail.com>
2024-05-08 22:35:27 +0000

    Event Listeners

---
+++ events.js
@@ -0,0 +1,9 @@
+const btn = document.querySelector("#btn");
+
+btn.onclick = () => alert("Hello World");
+
+const btn1 = document.querySelector("#btn1");
+
+btn1.addEventListener("click", () => {
+    alert("Hello World");
+})
