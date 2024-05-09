9a9cf264d0e719fcd558f22be981967c6bb78590
Majid Mohammed <majidmohammed044@gmail.com>
2024-05-09 10:25:44 +0000

    edited

--- events.js
+++ events.js
@@ -1,9 +1,10 @@
-const btn = document.querySelector("#btn");
+// // buttons is a node list. It looks and acts much like an array.
+// const buttons = document.querySelectorAll("button");
 
-btn.onclick = () => alert("Hello World");
-
-const btn1 = document.querySelector("#btn1");
-
-btn1.addEventListener("click", () => {
-    alert("Hello World");
-})
+// // we use the .forEach method to iterate through each button
+// buttons.forEach((button) => {
+//   // and for each one we add a 'click' listener
+//   button.addEventListener("click", () => {
+//     alert(button.id);
+//   });
+// });
